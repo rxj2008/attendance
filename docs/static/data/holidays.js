28c6484ae414842ca688debabb20a9baae1dcaf7
musishui <rxj_2008@foxmail.com>
2017-11-29 18:05:46 +0800

    重新发布

---
+++ docs/static/data/holidays.js
@@ -0,0 +1,105 @@
+export const holidays = [{
+  id: 20170122,
+  isWorkday: true
+},
+{
+  id: 20170204,
+  isWorkday: true
+},
+{
+  id: 20170401,
+  isWorkday: true
+},
+{
+  id: 20170527,
+  isWorkday: true
+},
+{
+  id: 20170930,
+  isWorkday: true
+},
+{
+  id: 20170101,
+  isWorkday: false
+},
+{
+  id: 20170102,
+  isWorkday: false
+},
+
+{
+  id: 20170127,
+  isWorkday: false
+},
+{
+  id: 20170128,
+  isWorkday: false
+},
+{
+  id: 20170129,
+  isWorkday: false
+},
+{
+  id: 20170130,
+  isWorkday: false
+},
+{
+  id: 20170131,
+  isWorkday: false
+},
+{
+  id: 20170201,
+  isWorkday: false
+},
+{
+  id: 20170202,
+  isWorkday: false
+},
+
+{
+  id: 20170403,
+  isWorkday: false
+},
+{
+  id: 20170404,
+  isWorkday: false
+},
+
+{
+  id: 20170501,
+  isWorkday: false
+},
+
+{
+  id: 20170529,
+  isWorkday: false
+},
+{
+  id: 20170530,
+  isWorkday: false
+},
+
+{
+  id: 20171001,
+  isWorkday: false
+},
+{
+  id: 20171002,
+  isWorkday: false
+},
+{
+  id: 20171003,
+  isWorkday: false
+},
+{
+  id: 20171004,
+  isWorkday: false
+},
+{
+  id: 20171005,
+  isWorkday: false
+},
+{
+  id: 20171006,
+  isWorkday: false
+}]
