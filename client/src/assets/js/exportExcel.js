ddb465c6f19709329ce44481bc81f748c5469cd8
musishui <rxj_2008@foxmail.com>
2017-11-29 19:44:37 +0800

    修改导出格式

--- client/src/assets/js/exportExcel.js
+++ client/src/assets/js/exportExcel.js
@@ -1,52 +1,72 @@
-import {excel} from './excel'
+import {
+    excel
+} from './excel'
 export function exportExcel(attendances) {
   var datas = []
-  let overTime = {name: '加班', data: [['序号', '姓名', '年/月/日', '时', '分', '年/月/日', '时', '分', '总时长', '事由', '本人手签']]}
+  let overTime = {
+    name: '加班',
+    data: [
+            ['姓名', '加班日期', '开始时间', '结束时间', '时长(小时)', '原因', '本人签字']
+    ]
+  }
   let overTimeIdx = 1
-  let leaves = {name: '调休', data: [['序号', '姓名', '年/月/日', '时', '分', '年/月/日', '时', '分', '总时长', '事由', '本人手签']]}
+  let leaves = {
+    name: '调休',
+    data: [
+            ['姓名', '休假日期', '开始时间', '结束时间', '休假时长(小时)', '休假类型', '本人签字']
+    ]
+  }
   let leaveIdx = 1
-  let reimburses = {name: '报销', data: [['序号', '所属分公司', '姓名', '报销日期', '金额', '票据发生类别', '票据发生时间', '出发地—目的地', '客户公司名称', '客户姓名及电话', '备注说明']]}
+  let reimburses = {
+    name: '报销',
+    data: [
+            ['序号', '所属分公司', '姓名', '报销日期', '金额', '票据发生类别', '票据发生时间', '出发地—目的地', '客户公司名称', '客户姓名及电话', '备注说明']
+    ]
+  }
   let reimburseIdx = 1
 
   attendances.forEach(att => {
     att.dates.forEach(date => {
-      // 加班
+            // 加班
       if (date.overtimes.length) {
         date.overtimes.forEach(ot => {
           let item = []
-          item.push(overTimeIdx++)
           item.push(att.name)
-          item.push(`${date.year}年${date.month}月${date.date}日`)
-          item.push(ot.startTime.hours)
-          item.push(ot.startTime.minutes)
-          item.push(`${date.year}年${date.month}月${date.date}日`)
-          item.push(ot.endTime.hours)
-          item.push(ot.endTime.minutes)
+          item.push(`${date.year}/${date.month}/${date.date}`)
+          item.push(new Date(date.year, date.month - 1, date.date, ot.startTime.hours, ot.startTime.minutes, 0))
+          // item.push( ot.startTime.hours)
+          // item.push(ot.startTime.minutes)
+          // item.push(`${date.year}年${date.month}月${date.date}日`)
+          // item.push(ot.endTime.hours)
+          // item.push(ot.endTime.minutes)
+          item.push(new Date(date.year, date.month - 1, date.date, ot.endTime.hours, ot.endTime.minutes, 0))
           item.push(ot.time.value / 60)
           item.push('')
           item.push('')
           overTime.data.push(item)
         })
       }
-      // 调休
+            // 调休
       if (date.leaves.length) {
         date.leaves.forEach(ot => {
           let item = []
-          item.push(leaveIdx++)
           item.push(att.name)
-          item.push(`${date.year}年${date.month}月${date.date}日`)
-          item.push(ot.startTime.hours)
-          item.push(ot.startTime.minutes)
-          item.push(`${date.year}年${date.month}月${date.date}日`)
-          item.push(ot.endTime.hours)
-          item.push(ot.endTime.minutes)
+          item.push(`${date.year}/${date.month}/${date.date}`)
+          item.push(new Date(date.year, date.month - 1, date.date, ot.startTime.hours, ot.startTime.minutes, 0))
+          item.push(new Date(date.year, date.month - 1, date.date, ot.endTime.hours, ot.endTime.minutes, 0))
+
+          // item.push(ot.startTime.hours)
+          // item.push(ot.startTime.minutes)
+          // item.push(`${date.year}年${date.month}月${date.date}日`)
+          // item.push(ot.endTime.hours)
+          // item.push(ot.endTime.minutes)
           item.push(ot.time.value / 60)
           item.push('调休')
           item.push('')
           leaves.data.push(item)
         })
       }
-      // 报销
+            // 报销
       if (date.reimburses.length) {
         date.reimburses.forEach(it => {
           let item = []
@@ -56,7 +76,9 @@ export function exportExcel(attendances) {
           item.push(`${date.year}/${date.month}/${date.date}`)
           item.push(it.money)
           item.push(it.type)
-          item.push(it.times.map(t => { return `${t.hours}:${t.minutes}` }).join('/'))
+          item.push(it.times.map(t => {
+            return `${t.hours}:${t.minutes}`
+          }).join('/'))
           item.push('')
           item.push('')
           item.push('')
